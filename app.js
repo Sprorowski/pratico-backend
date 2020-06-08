a570cdf0649ef5f61429b4625987f5f4bdb9a383
Guilherme Sprorowski <guilherme.97.gis@gmail.com>
2020-06-08 19:59:27 -0300

    start

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 var cors = require('cors')
 
-const basename = "accounts.json";
+const basename = "contacts.json";
 
 
 let errMsg = "";
@@ -12,7 +12,7 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-app.get('/account', function (req, res) { 
+app.get('/ccontact', function (req, res) { 
   res.send('Hello World!');
 });
 
@@ -22,17 +22,17 @@ app.post('/new', async (req, res)=>{
       console.log(errMsg);
     }
     let lastId = 0;
-    if(base.accounts.length){;
-      lastId = base.accounts[base.accounts.length - 1].id;
+    if(base.contacts.length){;
+      lastId = base.contacts[base.contacts.length - 1].id;
     } 
-    let account = {
+    let ccontact = {
       id: ++lastId,
-      titulo: req.body.titulo,
-      autor: req.body.autor,
-      editora: req.body.editora,
-      area: req.body.area
+      name: req.body.name,
+      email: req.body.email,
+      fone: req.body.fone,
+      adress: req.body.adress
     }
-    base.accounts.push(account);
+    base.contacts.push(ccontact);
     if(await updateBase(base)){        
       return res.status(201).send();;
     }else{
@@ -47,8 +47,8 @@ app.get('/', (req, res) =>{
     if(!base){
       console.log(errMsg);
     }           
-    if(base.accounts){      
-       return res.status(200).send(base.accounts);
+    if(base.contacts){      
+       return res.status(200).send(base.contacts);
     }
     return res.status(404).send({msg: `Erro`});
  
@@ -64,9 +64,9 @@ const readBase = () => new Promise((resolve, reject) => {
   fs.readFile(basename, 'utf8', (err, data) => {
     if (err){
       if(err.code === "ENOENT"){
-        fs.writeFile(basename, "{\"accounts\": []}" ,'utf8', function(err){ 
+        fs.writeFile(basename, "{\"contacts\": []}" ,'utf8', function(err){ 
           if(err)reject(err)
-          else resolve(JSON.parse("{\"accounts\": []}"))
+          else resolve(JSON.parse("{\"contacts\": []}"))
         });
       }else reject(err)        
     }        
